c3077c7c5aaebee41e9af96b288b36c1ef7b6253
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-02-14 06:19:26 -0700

    got some js going

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,21 @@
-// connection
-// majestic
-// spiky
-// overt
-// rigid
-// end
-// awake
-// limit
-// whimsical
-// courageous
+// Generate a random word
+// WHen the person it inputs to it needs to check if that letter is in the word or not
+// Letters appear
+// Number of guesses decreases
+// After 12 wrong guess says you lose
+// After guessing the correct word it says you win
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
   't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var word;
+var words = ["connection", "majestic", "spiky", "overt", "rigid", "end", "awake", "limit", "whimsical", "courageous"]
+var randomWord = Math.floor(Math.random() * words.length)
+
+var word = words[randomWord]
+console.log(word)
+
+
 
 var guess;
 
@@ -22,3 +24,7 @@ var guesses;
 var lives;
 
 var space;
+
+
+
+
