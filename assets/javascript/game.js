ca1aa9cc3c45a9106e14ea4061a759b4f93bed04
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-02-13 18:33:33 -0700

    minor touch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,8 +9,16 @@
 // whimsical
 // courageous
 
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+  't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 var word;
 
 var guess;
 
-var  
+var guesses;
+
+var lives;
+
+var space;
