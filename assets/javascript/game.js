23b8b24fd547ea1fedb81423f7e0bc5ba873c03a
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-02-18 14:41:17 -0700

    close but not exactly what I would like

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,17 @@
-// Generate a random word and the appropriate number of spaces for the word
-// WHen the person it inputs to it needs to check if that letter is in the word or not
-// Letters appear
-// Number of guesses decreases
-// After 12 wrong guess says you lose
-// After guessing the correct word it says you win
-
-
-// GIVEN AN ARRY OF SAVE USER GUESSES
-
-// GIVEN A CURRENT
-
-// GIVEN A CURRENT WORD TO CHECK AGAINST
-
-// THEN SHOW A STRING WITH CORRECT LETTERS
-
-// START WITH EMPTY RESULT STRING
-// LOOP THROUGH EACH LETTER OF THE WORD
-    //ADD UNDERSCORE TO RESULT
-    //LOOP THROUGH EACH CORRECT GUESS
-      //IF LETTER IS SAME AS CURRENT LETTER OF WORD REPLACE WITH LETTER
-
-//
-
-
 window.onload = function () {
 
-  var letterArray = [ ];
+  var letterArray = [];
 
-
-
-  // var splitLetterArray = letterArray.split()
-
-  guesses = 12
+  var guessesRemaining = 12;
 
   var words = ["connection", "majestic", "spiky", "overt", "rigid", "end", "awake", "limit", "whimsical", "courageous"]
+
+  // Guesses Remaining: Could get the guesses remaing to reduce by the number of spaces in the word on a key press if I put this within the scope of the function, but it wouldn't show from the start. Only after an initial key press.
+  document.getElementById("guessRemaining").innerHTML = "You have " + guessesRemaining + " guess(es) remaining";
+
+  if (guessesRemaining <= 0){
+    document.getElementById("guessRemaining").innerHTML = "Game Over";
+  }
 
   // Computer selects a random word
   var randomIndex = Math.floor(Math.random() * words.length);
@@ -46,11 +24,9 @@ window.onload = function () {
 
   var wordInPlay = "";
 
-  
   console.log(currentWord)
 
-
-// Current word is given spaces
+  // Current word is given spaces
   for (var i = 0; i < wordLength; i++) {
     var spaces = document.createElement("li");
     spaces.innerHTML = "_ ";
@@ -62,48 +38,32 @@ window.onload = function () {
     console.log(wordInPlay);
   }
 
-
- // Create a new variable that + or joins guessed letter every time key is pressed
- // look up how to use join()
- //console log the new variable to see if its working
- // figure out how to display the letters in variable
-  // Accepts users' letter inputs
+// Takes key inputs from user
   document.onkeyup = function (event) {
     var guessedLetter = event.key.toLowerCase();
-    console.log(guessedLetter);
-    // Before execute this line have to be sure user hasn't picked that letter already...if/if else statement{ 
-    letterArray.push(" " + guessedLetter); 
+    // My if/else statement wouldn't work properly in here to alert the user if they picked a letter more than once. 
+    letterArray.push(" " + guessedLetter);
     console.log(letterArray);
     document.getElementById("letters").innerHTML = letterArray;
-    
-  // }
 
-    for(var j = 0; j < wordInPlay.length; j++) {
-      if (correctWord[j] === guessedLetter) { 
+    // }
+
+    for (var j = 0; j < wordInPlay.length; j++) {
+      if (correctWord[j] === guessedLetter) {
         wordInPlay[j] = guessedLetter;
-       var lisInDom = document.querySelectorAll(".spaced");
-       lisInDom[j].innerText = guessedLetter + " ";
+        var lisInDom = document.querySelectorAll(".spaced");
+        lisInDom[j].innerText = guessedLetter + "  ";
       }
-    }
 
-
-
-  }
-
-
-  
-
-
-  // Guesses Remaining
-  document.getElementById("guessRemaining").innerHTML = "You have " + guesses + " guess(es) remaining";
-  if (guesses <= 0) {
-    document.getElementById("guessRemaining").innerHTML  = "Game Over";
-  }
-
-  // for ( j = 0; j == currentWord; j++) {
-   
+      //Supposed to remove 1 guess from the guessesRemaining variable, but it's not in the scope of that variable to make the change. In addition it counts wrong and right answers = to the number of spaces that it's checking. E.g. If the word has 3 letters then it's 3 less guesses
+       else if (wordInPlay[j] !== guessedLetter) {
+        guessesRemaining--;
+        console.log(guessesRemaining)
+         }
+      }
     
-
+  }
+ 
 }
 
 
